43e9c5fc47ebab6c197b96bdb408b199fb154cd6
cbeauvoi <cyprian@bluesquare.io>
2019-02-20 12:28:19 +0100

    debug

--- StorageBundle/DependencyInjection/StorageExtension.php
+++ StorageBundle/DependencyInjection/StorageExtension.php
@@ -4,6 +4,7 @@ namespace Bluesquare\StorageBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
