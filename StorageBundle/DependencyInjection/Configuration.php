5d8efab159517b88d0a6b5a3a7ebbc5463ca0f64
cbeauvoi <cyprian@bluesquare.io>
2019-02-21 12:55:49 +0100

    debug change directory

--- StorageBundle/DependencyInjection/Configuration.php
+++ StorageBundle/DependencyInjection/Configuration.php
@@ -16,7 +16,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $root = $treeBuilder->root('storage');
+        $root = $treeBuilder->root('bluesquare/storage');
         $root->useAttributeAsKey('storage_name')
             ->prototype('array')
                 ->children()
